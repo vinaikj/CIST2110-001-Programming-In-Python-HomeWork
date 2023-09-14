9ed045b3784063b39bb78a384ef1b4da585fbf5f
walshg3 <walshg3@go.stockton.edu>
2023-09-14 17:43:46 -0400

    added HW1

---
+++ HW1.py
@@ -0,0 +1,41 @@
+# HW1.py
+# Author:
+
+# Question 1:
+# Print Hello World like we did in class
+
+# Question 2:
+# Print the following:
+# Your name
+# Your age
+# Your favorite color
+# Your favorite animal
+
+# Question 3:
+# Create a variable called "myName" and set it equal to your name
+# Create a variable called "myAge" and set it equal to your age
+# Create a variable called "myColor" and set it equal to your favorite color
+# Create a variable called "myAnimal" and set it equal to your favorite animal
+# Print the following:
+# Hello, my name is <myName>
+# I am <myAge> years old
+# My favorite color is <myColor>
+# My favorite animal is <myAnimal>
+
+# Question 4:
+# Calculate the following and print the result:
+# 2 + 2
+# 3 * 4
+# 5 - 6
+# 8 / 2
+
+# Question 5:
+# Create a variable called "num1" and set it equal to 2
+# Create a variable called "num2" and set it equal to 3
+# Create a variable called "num3" and set it equal to 4
+# Create a variable called "num4" and set it equal to 5
+# Calculate the following and print the result:
+# num1 + num2
+# num3 * num4
+# num4 - num1
+# num2 / num1
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CIST2110-001-Programming-In-Python-HomeWork
+
+# This is a repository for my homework assignments for CIST 2110-001 Programming in Python
