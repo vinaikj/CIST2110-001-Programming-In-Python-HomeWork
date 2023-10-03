79261eae78c824ea8ed63be949cb16c4ff19dd21
vinaikj <vinaikj@go.stockton.edu>
2023-10-03 09:23:08 -0400

    Homework 2 completed.

--- HW2.py
+++ HW2.py
@@ -1,13 +1,22 @@
 # HW2.py
-# Author:
+# Author: Jayant Vinaik
 
 
 # Question 1:
 # Write some code that prompts the user for their age. Depending on the input:
 
+
 # If the age is less than 13, print "You are a child."
 # If the age is between 13 and 19, print "You are a teenager."
 # If the age is 20 or older, print "You are an adult."
+
+age= float(input("Enter your age: "))
+if age < 13:
+    print("You are a child.")   
+elif age >= 13 and age <= 19:   
+    print("You are a teenager.")            
+elif age >= 20:
+    print("You are an adult.")
 
 
 # Question 2:
@@ -18,6 +27,11 @@
 # 1234
 # 12345
 
+temp = ""
+for x in range(1,6):
+    temp = temp + str(x)
+    print(temp) 
+
 # Question 3:
 # Write a Python program that prompts the user to input 10 numbers. After all the numbers are inputted, the program should display:
 
@@ -25,8 +39,26 @@
 # The lowest number.
 # The average of all the numbers.
 
+numbers = [0,0,0,0,0,0,0,0,0,0]
+for x in range(10):
+ numbers[x] = int(input("Enter number "+ str(x+1) + ": "))  
+
+print("The highest number is " + str(max(numbers))) 
+print("The lowest number is " + str(min(numbers)))      
+print("The average of all the numbers is " + str(sum(numbers)/len(numbers)))    
+
+
+
 # Question 4:
 # Vowel Counter - Write some code that prompts the user to enter a string. The program should then display the number of vowels in the string. IE. If the user enters "Hello World", the program should display 3.
 # the vowels are a, e, i, o, u
 # Hint: convert the string to lowercase and use a for loop with a counter variable and an if statement
 
+userEnter = str(input("Enter a string: "))
+userEnter.lower()
+
+counter = 0
+for x in userEnter:
+    if (x == "a" or x == "e" or x == "i" or x == "o" or x == "u"):
+        counter = counter + 1           
+print("The number of vowels in the string is " + str(counter))  
