0746bae2daa70b4720d698dd9a89114d97f12ca7
vinaikj <vinaikj@go.stockton.edu>
2023-10-16 15:12:45 -0400

    Project 1 completed

--- Project1.py
+++ Project1.py
@@ -1,36 +1,67 @@
 # Project1.py
-# Author:
+# Author: Jayant Vinaik
 
 
-# This project is meant to test your ability from everything we have learned so far in class
-# You will need to use variables, if statements, loops, and functions
 
-# Quiz Game:
-# Create a simple console-based quiz game where the user answers a series of questions.
-# The game should keep track of the user's score and provide feedback based on the answers given.
+#farewell message
+def farewell(score): 
+    print("Thank you for playing!")
+    print("Your score is " + str(score) + " out of 7.") 
 
-# Write a function that displays a welcome message to the user and explains the rules of the game
+#welcome message
+def Welcome(): 
+    print("Welcome to the quiz game!")  
+    print("You will be asked a series of questions.")
+    print("There are 7 questiongs each will have 4 answer options (a, b, c, d).")
+    print("Enter quit to end the game and exit")
 
-# Implement at least 5 questions, each with 4 answer options (a, b, c, d). Each question should be worth 1 point.
-# For each question, display the question and the answer options to the user.
-# Use input() to get the user's answer.
-# Use if or if-else statements to check if the answer is correct.
-# If the answer is correct, display a positive feedback message and add points to the user's score.
-# If the answer is incorrect, display a negative feedback message and provide the correct answer.
-# Score Tracking:
+#Ask question function  
+def ask_question(question, option_1, option_2, option_3, option_4, correct_answer):
+    print(question)
+    print("a. " + option_1)
+    print("b. " + option_2)
+    print("c. " + option_3)
+    print("d. " + option_4)
+    user_answer = str(input("Enter your answer: "))
+    if(user_answer == correct_answer):
+        print("Correct!")   
+        return True
+    elif(user_answer == "a" or user_answer == "b" or user_answer == "c" or user_answer == "d"): 
+        print("Incorrect!")
+        return False   
+    elif (user_answer == "quit"):
+        print("Thank you for playing!")
+        exit()
+    else: 
+        print("Invalid input, enter correct answer")
+        ask_question(question, option_1, option_2, option_3, option_4, correct_answer) 
 
-# Keep track of the user's score throughout the game.
-# After all questions have been answered, display the user's total score and a farewell message.
-# Function Utilization:
+#main function 
+def main():
+    score = 0
 
-# Create a function to ask a question and check the answer. This function should accept parameters like the question, options, and the correct answer, and return whether the user was correct.
-# an example would be def ask_question(question, option_1, option_2, option_3, option_4, correct_answer):
-# the return value should be a boolean (True or False) for whether the user was correct
+    Welcome()
 
-# Create a function to display the final score, which takes the score as a parameter and displays a message.
-# Loops:
-# Use a for or while loop to iterate through the questions.
-# Variable Casting:
-# Ensure that user input is cast and checked appropriately to avoid errors during execution.
-# Error Handling:
-# Implement basic error handling to manage invalid inputs from the user (e.g., an answer other than a, b, c, or d).
+    if(ask_question("Question 1: What is the capital of India?", "Mumbai", "New Delhi", "Kolkata", "Chennai", "b")):    
+        score += 1  
+    if(ask_question("Question 2: What is the national dish of India?", "Pizza", "Chicken Tikka", "Khichdi", "French Fries", "c")):     
+        score += 1
+    if(ask_question("Question 3: What is the official language of India?", "No offical language", "Hindi", "French", "Hindi", "b")):     
+        score += 1  
+    if(ask_question("Question 4: What is the name of Indian Prime Minister?", "Obama", "Narendra Modi", "Sadhguru", "Tom Brady", "b")):
+        score += 1
+    if(ask_question("Question 5: What is the festival of lights called?", "Diwali", "Holi", "Ramadan", "Christmas", "a")):
+        score += 1  
+    if(ask_question("Question 6: What is the popular form of music and dance from Punjab?", "Bhangra", "Raga", "Sattriya", "Tap dancing", "a")):
+        score += 1  
+    if(ask_question("Question 7: What is the capital of Japan?", "Tokyo", "Osaka", "Kyoto", "Yokohama", "a")):
+        score += 1  
+    if(ask_question("Question 8: What is the capital of Russia?", "Moscow", "Saint Petersburg", "Novosibirsk", "Yekaterinburg", "a")):
+        score += 1  
+
+
+
+    farewell(score)
+
+if __name__ == "__main__":
+    main()  
