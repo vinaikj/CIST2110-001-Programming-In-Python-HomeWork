8a04a9fc1333edbe5b91f0011bec93816ae2c0d6
vinaikj <vinaikj@go.stockton.edu>
2023-10-19 14:09:57 -0400

    hw 3 completed

--- HW3.py
+++ HW3.py
@@ -1,39 +1,64 @@
 # HW3.py
-# Author:
+# Author:Jayant vinaik  
 
 # This Homework assignment is meant to test your ability to make functions within python as well as importing and using modules. This assignment might require you to do some research on your own. If you get stuck, try googling the problem, especially when it comes to importing and using the different modules.
 
-# Question 1:
-# Write a function that takes in a number and returns that number squared
-# IE. If the user inputs 3, the function should return 9
+import random
+import math
+import sys as system
+import os
 
-# Question 2:
-# Write a function that takes in a string, a letter, and a number and returns the string with the letter replaced at the number index
-# IE. If the user inputs "Hello World", "a", and 3, the function should return "Helao World"
+def square_number(number):
+    return number ** 2
 
-# Question 3:
-# Write a function that takes in a number, a lower bound, and an upper bound and returns whether the number is within the bounds
-# IE. If the user inputs 5, 1, and 10, the function should return True
+def replace_char_at_index(input_string, replacement_char, index):
+    if index < 0 or index >= len(input_string):
+        return input_string  # Index out of range, return the original string unchanged
+    else:
+        return input_string[:index] + replacement_char + input_string[index + 1:]
 
-# Question 4:
-# Write a function that asks the user for their name, age, and favorite color. Then write a function that accepts those three parameters and prints them out in a sentence
-# IE. If the user inputs "John", 20, and "blue", the function should print "Hello, my name is John. I am 20 years old. My favorite color is blue."
-# Hints: You will need to use the input() function to get the user's input. You will also need to use the str() function to convert the age to a string
-# This is a two part question. You will need to write two functions
-# remember in class we learned you can return miltiple values from a function
-# also remember in class you can pass in pultiple variables into a function
+def is_within_bounds(number, lower_bound, upper_bound):
+    return lower_bound <= number <= upper_bound
 
-# Question 5:
-# import the random module and use it to generate a random number between 1 and 100
+def get_user_info():
+    name = input("Please enter your name: ")
+    age = int(input("Please enter your age: "))
+    color = input("Please enter your favorite color: ")
+    return name, age, color
 
-# Question 6:
-# import the math module and use it to find the square root of 16 (hint: use the sqrt() function)
+def print_user_info(name, age, color):
+    print(f"Hello, my name is {name}. I am {str(age)} years old. My favorite color is {color}.")
 
-# Question 7:
-# import the sys module and use it to display the version of python you are using
-# this time import the module using the import "as" keyword
-# hint: use the version attribute (sys.version)
+def generate_random():
+    random_number = random.randint(1, 100)
+    print
 
-# Question 8:
-# import the os module and use it to display the current working directory. This time import the module using the from keyword
-# hint: use the getcwd() function
+def find_square_root(number):
+    square_root = math.sqrt(number)
+    print("The square root of " + str(number) + " = " + str(square_root))
+
+
+def display_version():
+    python_version = system.version
+    print("Python Version:", python_version)
+
+
+def display_cwd():
+    current_directory = os.getcwd()
+    print("Current Working Directory:", current_directory)
+
+def main():
+    # Your main logic goes here
+   print (square_number(16)  )  
+   print(replace_char_at_index("Good Evening", "!", 2)  ) 
+   print(is_within_bounds(5, 1, 10)  ) 
+   get_user_info() 
+   print_user_info("Jayant", 21, "Blue")   
+   generate_random()   
+   find_square_root(16)   
+   display_version()    
+   display_cwd()   
+
+# Call the main function if the script is run directly
+if __name__ == "__main__":
+    main()
