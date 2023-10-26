0d46ac4ae76bb77133184a45c390db1545187ff6
walshg3 <walshg3@go.stockton.edu>
2023-10-26 17:19:21 -0400

    added hw 5

---
+++ HW5.py
@@ -0,0 +1,28 @@
+# HW5.py
+# Author:
+
+# This homework assignment tests on list in python
+
+# Question 1: Create a list with 5 of your favorite foods. Print the list
+
+# Question 2: Using the list from question 1, print the first and last element of the list
+
+# Question 3: Using the list from question 1, print the 3rd element of the list
+
+# Question 4: Using the list from question 1, print the 1st through 3rd elements of the list using list slicing
+
+# Question 5: Using the list from question 1, print the last 2 elements of the list using list slicing
+
+# Question 6: Using the list from question 1, create a for each loop that prints each element of the list
+
+# Question 7: Using the list from question 1, create a for loop that prints each element of the list in reverse order
+
+# Question 8: Using the list from question 1, create a for loop that prints each element of the list and its index (hint use the enumerate() function)
+
+# Question 9: Using this list of lists, print the first element of the second list (hint: use indexing)
+list = [[1,2,3],[4,5,6],[7,8,9]]
+
+
+# Question 10: Create a function that will take in a list and return the list in reverse order
+# Hint: You can take in a list as a parameter and return a list
+
